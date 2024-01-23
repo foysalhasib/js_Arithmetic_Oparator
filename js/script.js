563e22b06f3cd92b64f6b7970d3171115d189073
Foysal Hasib <118272136+foysalhasib@users.noreply.github.com>
2024-01-24 01:00:37 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// ===============Increment start================
+
+var a = 10;
+var b = 20;
+
+   a++
+   ++b
+
+
+console.log(a)
+console.log(b)
+
+// ===============Increment end==================
+
+
+
+//===============Increment_different start================
+  var a=100;
+  var b=50;
+  var c,d;
+
+    c = a++;  //---------pore man bare---------
+    d = ++b;  //---------age man bare tarpor store hoy---------
+
+
+  console.log(c)
+  console.log(a)
+  console.log(d)
+
+// ===============Increment_different end==================
+
+
+
+
+// =============================minus=============================
+var a = 10;
+var b = 20;
+
+   a--
+   --b
+
+
+console.log(a)
+console.log(b)
